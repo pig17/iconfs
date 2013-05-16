f95692f99f8074ef063f073f0c578445e8a86695
Santoz5 <m.angelo.safara@gmail.com>
2013-05-16 16:49:10 +0100

    correccao events e ligacoes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,45 +1,18 @@
 class Event < ActiveRecord::Base
-  attr_accessible :time, :title, :duration, :type
+  attr_accessible :time, :title, :document, :user, :durationM
 
   belongs_to :room
   belongs_to :document
   belongs_to :track
-  belongs_to :day
-  belongs_to :user
   has_and_belongs_to_many :schedules
 
+  validates :title, :presence => true, :uniqueness => true
   validates :time, :presence => true
-  validates :title, :presence => true
-  validates :duration, :presence => true
-  validates :type, :presence => true
-  validates :room_id, :presence => true
-  #validates :document_id, :presence => true, :if => type != "social"  ALTERAR CASO NAO SE PERMITA QUE EV SOCIAIS TENHAM DOCUMENTOS
-  validates :day_id, :presence => true
 
-  # User is always one of the authors
-  validate :speaker_is_an_author
+  #has_one :document  #belongs_to or has_on ??
+  #has_one :user, :dependent => :destroy
+  #belongs_to :track, :foreign_key => :track_id
+  #has_one  :room, :through => :events
 
-  # Type must be one of the default set
-  validate :correct_type
-
-  # Each time appears only one time per (room,day)
-  validates_uniqueness_of :time, :scope => [:room_id, :day_id]
-
-  # Each title appears only one time per track
-  validates_uniqueness_of :title, :scope => :track_id
-
-
-
-  def speaker_is_an_author
-    unless self.document.users.all.include? self.user
-      errors.add(:user_id, "speaker must be an author")
-    end
-  end
-
-  def correct_type
-    unless ["keynote","workshop","paper presentation","poster presentation","social"].include? self.type
-      errors.add(:type, "incorrect type")
-    end
-  end
 
 end
