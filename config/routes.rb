7a4330d2a7e73d0bc66ae2c5dc39654f61fc0b89
Andreia Marques <af.marques@campus.fct.unl.pt>
2013-05-20 21:16:05 +0100

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Iconfs::Application.routes.draw do
+
   devise_for :users
 
   resources :schedules_events
@@ -31,27 +32,16 @@ Iconfs::Application.routes.draw do
   resources :schedules
 
 
-  get 'admin/inipage'
-
   resources :pis
 
 
   resources :rooms
 
 
-  resources :events
-
-
-  resources :events
-
-
   resources :documents
 
 
-  resources :users
-
-
-  resources :notes
+   resources :notes
 
 
   resources :events
@@ -60,60 +50,8 @@ Iconfs::Application.routes.draw do
   resources :users
 
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  get 'admin/inipage'
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
   root :to => 'admin#inipage'
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
